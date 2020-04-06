e01d963e76b2d7d1f1ee17fc7c86232dbb445bce
Lea Vanheyden <lea.schulz-vanheyden@gmx.de>
2020-04-06 15:10:17 +0200

    fill sociomatrix

--- generate_network.R
+++ generate_network.R
@@ -9,8 +9,12 @@ gen_net <- function(n = 100, # number of nodes
   # sociomatrix to be filled
   sociomatrix <- matrix(data = NA, nrow = n, ncol = n)
   
+  # position is 1 in beginning
+  position_start <- 1
+  
   for(i in 1:G) {
     G_size_i <- G_size[i] # get size of group
+    position_end <- position_start + G_size_i -1
     
     # get edges for in group
     G_edges_i <- sample(c(0, 1), G_size_i*G_size_i, replace = TRUE, 
@@ -21,9 +25,13 @@ gen_net <- function(n = 100, # number of nodes
                       prob = c((1 - betw_dens), betw_dens))
     
     # fill the sociomatrix
-    
+    sociomatrix[position_start:position_end,
+                position_start:position_end] <- G_edges_i
+    sociomatrix[position_start:position_end,
+                -(position_start:position_end)] <- betw_edges_i
     
     # continue  with next group
+    position_start <- position_end + 1
     i <- i+1
   }
 }
