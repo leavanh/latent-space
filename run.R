bf09241b9690c569792d150f4f30d5d7947b356d
Lea Schulz-Vanheyden <ru32vux2@production-rstudio-1.novalocal>
2020-04-21 12:39:06 +0200

    generate all networks

--- run.R
+++ run.R
@@ -9,9 +9,13 @@ library(tidyverse)
 source("functions.R")
 
 
-# generate networks
+## generate networks
 
 set.seed(09101999)
+
+# make the networks
+# 10, 50 and 100 nodes
+# up to 6 dimensions
 
 p <- rsphere(10, dim = 6)
 n <- gen_network(p)
