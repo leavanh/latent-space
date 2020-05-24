dde289065fa1d5e65c2157317718b8be74e81e1c
leavanh <elektronische_post@gmx.de>
2020-05-24 12:23:57 +0200

    plot all together

--- visualize.R
+++ visualize.R
@@ -2,16 +2,19 @@
 
 # load packages
 library(RColorBrewer)
+library(cowplot)
 
 theme_set(theme_bw()) # set theme
 c_palette <- c("#FEE090", "#FDAE61", "#F46D43", "#D73027")
 
 # make all Plots in a loop
 
-for(n_nodes in 1:nlevels(unif_df$nodes)) {
-  n <- as.integer(levels(unif_df$nodes)[[n_nodes]])
+plots <- list()
+
+for(j in 1:nlevels(unif_df$nodes)) {
+  n <- as.integer(levels(unif_df$nodes)[[j]])
   subset_data <- subset(unif_df, nodes == n)
-  print(ggplot(subset_data,
+  plot <- ggplot(subset_data,
       aes(fit_dim, distance_diff, color = org_dim, group = org_dim)) +
     geom_point() +
     geom_line() +
@@ -20,6 +23,11 @@ for(n_nodes in 1:nlevels(unif_df$nodes)) {
     labs(title = paste(n, "nodes"),
          x = "Fitted dimension",
          y = "Difference of distances",
-         color = "Original dimension")
-  )
+         color = "Original \ndimension")
+  legend <- get_legend(plot)
+  plots[[j]] <- plot + 
+    theme(legend.position = "none") +
+    theme(plot.margin = margin(6, 0, 6, 0))
 }
+
+plot_grid(plotlist = plots, legend, nrow = 1)
