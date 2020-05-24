bde6a8cf93a4fc6b8fa4aaef990cbef2b5927757
leavanh <elektronische_post@gmx.de>
2020-05-24 10:44:50 +0200

    make results df

--- run.R
+++ run.R
@@ -20,38 +20,44 @@ set.seed(2020)
 # unif and normal distribution
 # use mle
 
-start_time <- Sys.time()
+# start_time <- Sys.time()
+# 
+# simulation_unif <- gen_fit_all(n = c(20, 50, 100), dim = c (2, 4, 6, 8), 
+#                                distribution = "unif", tofit = "mle")
+# save(simulation_unif, file = "simulation_unif.RData") # save
+# 
+# end_time <- Sys.time()
+# time_diff_unif <- end_time-start_time
+# 
+# start_time <- Sys.time()
+# 
+# simulation_normal <- gen_fit_all(n = c(20, 50, 100), dim = c (2, 4, 6, 8), 
+#                                distribution = "normal", tofit = "mle")
+# save(simulation_normal, file = "simulation_normal.RData")
+# 
+# end_time <- Sys.time()
+# time_diff_normal <- end_time-start_time
+# 
+# start_time <- Sys.time()
+# 
+# simulation_groups <- gen_fit_all(n = c(20, 50, 100), dim = c (2, 4, 6, 8), 
+#                                  distribution = "groups", n_groups = 3,
+#                                  tofit = "mle")
+# save(simulation_groups, file = "simulation_groups.RData")
+# 
+# end_time <- Sys.time()
+# time_diff_groups <- end_time-start_time
+# 
+# time_diff_unif
+# time_diff_normal
+# time_diff_groups
 
-simulation_unif <- gen_fit_all(n = c(20, 50, 100), dim = c (2, 4, 6, 8), 
-                               distribution = "unif", tofit = "mle")
-save(simulation_unif, file = "simulation_unif.RData") # save
-
-end_time <- Sys.time()
-time_diff_unif <- end_time-start_time
-
-start_time <- Sys.time()
-
-simulation_normal <- gen_fit_all(n = c(20, 50, 100), dim = c (2, 4, 6, 8), 
-                               distribution = "normal", tofit = "mle")
-save(simulation_normal, file = "simulation_normal.RData")
-
-end_time <- Sys.time()
-time_diff_normal <- end_time-start_time
-
-start_time <- Sys.time()
-
-simulation_groups <- gen_fit_all(n = c(20, 50, 100), dim = c (2, 4, 6, 8), 
-                                 distribution = "groups", n_groups = 3,
-                                 tofit = "mle")
-save(simulation_groups, file = "simulation_groups.RData")
-
-end_time <- Sys.time()
-time_diff_groups <- end_time-start_time
-
-time_diff_unif
-time_diff_normal
-time_diff_groups
-
-# load("simulation_unif.RData") # load
+load("simulation_unif.RData") # load
+load("simulation_normal.RData") # load
+load("simulation_groups.RData") # load
 
 unif_df <- prod_df(simulation_unif, "unif")
+normal_df <- prod_df(simulation_normal, "normal")
+groups_df <- prod_df(simulation_groups, "groups")
+
+results_df <- bind_rows(unif_df, normal_df, groups_df)
