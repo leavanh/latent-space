f629e46790bad7984215f7a766f7a447f200e0d6
leavanh <elektronische_post@gmx.de>
2020-05-24 12:49:27 +0200

    visualize difference of distances

--- visualize.R
+++ visualize.R
@@ -1,29 +1,17 @@
 ## Here I visualize the results
 
 theme_set(theme_bw()) # set theme
-c_palette <- c("#FEE090", "#FDAE61", "#F46D43", "#D73027")
+diff_palette <- c("#FEE090", "#FDAE61", "#F46D43", "#D73027")
 
-# make all Plots in a loop
-
-plots <- list()
-
-for(i in 1:nlevels(unif_df$nodes)) {
-  n <- as.integer(levels(unif_df$nodes)[[j]])
-  subset_data <- subset(unif_df, nodes == n)
-  plots[[i]] <- ggplot(unif_df,
-                 aes(fit_dim, distance_diff, color = org_dim, group = org_dim)) +
-    geom_point() +
-    geom_line() +
-    facet_grid(.~ nodes) +
-    scale_color_manual(values = c_palette) +
-    ylim(c(0, 32)) +
-    labs(title = "Unif distribution",
-         x = "Fitted dimension",
-         y = "Difference of distances",
-         color = "Original \ndimension")
-}
-
-
-
-
+ggplot(results_df,
+       aes(fit_dim, distance_diff, color = org_dim, group = org_dim)) +
+  geom_point() +
+  geom_line() +
+  facet_grid(distribution ~ nodes) +
+  scale_color_manual(values = diff_palette) +
+  ylim(c(0, 32)) +
+  labs(title = "Results: Difference of distances",
+       x = "Fitted dimension",
+       y = "Difference of distances",
+       color = "Original \ndimension")
 
