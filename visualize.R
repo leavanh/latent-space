a704c26c073c1a2ad93a68b8d2d747fef36dd7a7
Lea Schulz-Vanheyden <ru32vux2@production-rstudio-1.novalocal>
2020-07-20 09:15:52 +0200

    visualize new results

--- visualize.R
+++ visualize.R
@@ -1,15 +1,17 @@
 ## Here I visualize the results
 
 theme_set(theme_bw()) # set theme
-diff_palette <- c("#FEE090", "#FDAE61", "#F46D43", "#D73027")
-time_palette <- c("#ABD9E9", "#74ADD1", "#4575B4", "#313695")
+dist_palette <- c("#FEE090", "#FDAE61", "#F46D43", "#D73027")
+net_palette <- c("#ABD9E9", "#74ADD1", "#4575B4", "#313695")
+
+# mean distance of differences
 
 ggplot(results_mean_df,
        aes(fit_dim, mean_distance_diff, color = org_dim, group = org_dim)) +
   geom_point() +
   geom_line() +
   facet_grid(distribution ~ nodes) +
-  scale_color_manual(values = diff_palette) +
+  scale_color_manual(values = dist_palette) +
   #ylim(c(0, 30)) +
   labs(title = "Results: Difference of distances",
        x = "Fitted dimension",
@@ -17,39 +19,47 @@ ggplot(results_mean_df,
        color = "Original \ndimension")
 ggsave(file = "Did_mean.pdf", width = 210, height = 297, units = "mm")
 
+# sd difference of distances
+
 ggplot(results_mean_df,
        aes(fit_dim, sd_distance_diff, color = org_dim, group = org_dim)) +
   geom_point() +
   geom_line() +
   facet_grid(distribution ~ nodes) +
-  scale_color_manual(values = diff_palette) +
+  scale_color_manual(values = dist_palette) +
   #ylim(c(0, 30)) +
   labs(title = "Variance of the difference of distances",
        x = "Fitted dimension",
        y = "Standard deviation",
        color = "Original \ndimension")
-ggsave(file="Did_sd.pdf", width = 210, height = 297, units = "mm")
+ggsave(file = "Did_sd.pdf", width = 210, height = 297, units = "mm")
+
+# mean difference of networks
 
 ggplot(results_mean_df,
-       aes(fit_dim, mean_time, color = org_dim, group = org_dim)) +
+       aes(fit_dim, mean_network_diff, color = org_dim, group = org_dim)) +
   geom_point() +
   geom_line() +
   facet_grid(distribution ~ nodes) +
-  scale_color_manual(values = time_palette) +
+  scale_color_manual(values = net_palette) +
   # ylim(c(0, 60)) +
-  labs(title = "Results: Time to fit",
+  labs(title = "Results: Difference of networks",
        x = "Fitted dimension",
-       y = "Mean time (in seconds)",
+       y = "Mean difference",
        color = "Original \ndimension")
+ggsave(file = "Din_mean.pdf", width = 210, height = 297, units = "mm")
+
+# sd difference of networks
 
 ggplot(results_mean_df,
-       aes(fit_dim, sd_time, color = org_dim, group = org_dim)) +
+       aes(fit_dim, sd_network_diff, color = org_dim, group = org_dim)) +
   geom_point() +
   geom_line() +
   facet_grid(distribution ~ nodes) +
-  scale_color_manual(values = time_palette) +
+  scale_color_manual(values = net_palette) +
   # ylim(c(0, 60)) +
-  labs(title = "Variance of Time to fit",
-       x = "Fitted dimension",
-       y = "Standard deviation",
-       color = "Original \ndimension")
+  labs(title = "Variance of the difference of networks",
+        x = "Fitted dimension",
+        y = "Standard deviation",
+        color = "Original \ndimension")
+ggsave(file = "Din_sd.pdf", width = 210, height = 297, units = "mm")
